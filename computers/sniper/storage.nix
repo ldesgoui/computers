987c05483573e152734cec7bf35f76c78b782a4c
ldesgoui <ldesgoui@gmail.com>
2024-10-16 00:26:00 +0200

    chore(sniper): limit rollbacks to 8 configurations

--- computers/sniper/storage.nix
+++ computers/sniper/storage.nix
@@ -1,6 +1,9 @@
 {
   boot.loader = {
-    systemd-boot.enable = true;
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 8;
+    };
     efi.canTouchEfiVariables = true;
   };
 
