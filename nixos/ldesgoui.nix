2d2be2ceb3d416c8cdafd28a71586cd59c5affc2
ldesgoui <ldesgoui@gmail.com>
2024-12-04 16:35:29 +0100

    feat(ldesgoui): install inhibridge

--- nixos/ldesgoui.nix
+++ nixos/ldesgoui.nix
@@ -314,6 +314,15 @@ in
           };
         };
 
+        systemd.user.services.inhibridge = {
+          Unit = {
+            Description = "Inhibridge daemon";
+            PartOf = [ "graphical-session.target" ];
+          };
+          Service.ExecStart = lib.getExe pkgs.inhibridge;
+          Install.WantedBy = [ "graphical-session.target" ];
+        };
+
         wayland.windowManager.sway = {
           enable = true;
 
