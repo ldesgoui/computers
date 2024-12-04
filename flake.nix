6d9923062822c9d0f7c9abff76f9d94d7aba0fa4
ldesgoui <ldesgoui@gmail.com>
2024-12-04 16:18:43 +0100

    chore: update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Configurations for computers I operate";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-parts = {
@@ -27,7 +27,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
