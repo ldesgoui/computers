58bb3d62fcf7daa4b76b75abaf2ffd83091d6ccc
ldesgoui <ldesgoui@gmail.com>
2024-12-12 02:43:35 +0100

    fix(soldier): ignore some package EOLs to make sonarr run

--- computers/soldier/media-server.nix
+++ computers/soldier/media-server.nix
@@ -14,6 +14,14 @@
   services.sonarr = {
     enable = true;
   };
+  # FIXME: ^ sonarr causing a build error because of its deps
+  nixpkgs.config.permittedInsecurePackages = [
+    "dotnet-runtime-6.0.36"
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
+    "dotnet-sdk-wrapped-6.0.428"
+  ];
 
   services.lidarr = {
     enable = true;
