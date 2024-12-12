2ca1d0a3c07dd3b65cb9b2d13920244bd98c4304
ldesgoui <ldesgoui@gmail.com>
2024-12-12 01:20:09 +0100

    fix(soldier): fix hostid

--- computers/soldier/storage.nix
+++ computers/soldier/storage.nix
@@ -13,7 +13,7 @@
     fsType = "vfat";
   };
 
-  networking.hostId = "26afaa70"; # For ZFS
+  networking.hostId = "22222222"; # For ZFS
 
   swapDevices = [{
     device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNX0NA71896Y-part2";
