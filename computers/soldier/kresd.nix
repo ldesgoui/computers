4f2269dbd99e865afade0ac74a91778199318864
ldesgoui <ldesgoui@gmail.com>
2024-12-24 13:45:16 +0100

    fix(kresd): try without predict

--- computers/soldier/kresd.nix
+++ computers/soldier/kresd.nix
@@ -3,17 +3,13 @@
 
   services.kresd = {
     enable = true;
-    instances = 2;
-    listenPlain = [ "0.0.0.0:53" "[::]:53" ];
+    listenPlain = [
+      "lo:53"
+      "tailscale0:53"
+      "wg0:53"
+    ];
     extraConfig = ''
       cache.size = 100 * MB
-
-      modules = {
-        predict = {
-          window = 60,
-          period = 24
-        }
-      }      
     '';
   };
 }
