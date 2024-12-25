6f4037e9c8e411abfa0138983a4245b5967a78aa
ldesgoui <ldesgoui@gmail.com>
2024-12-25 01:36:49 +0100

    fix(flaresolverr): don't expose

--- computers/soldier/flaresolverr.nix
+++ computers/soldier/flaresolverr.nix
@@ -1,14 +1,7 @@
-{ config, pkgs, inputs, ... }: {
+{ pkgs, inputs, ... }: {
   services.flaresolverr = {
     enable = true;
     # TODO: when flaresolverr is fixed (xdd)
     package = inputs.nur-xddxdd.packages.${pkgs.stdenv.hostPlatform.system}.flaresolverr-21hsmw;
-  };
-
-  services.nginx.virtualHosts."flaresolverr.int.lde.sg" = {
-    enableACME = true;
-    acmeRoot = null;
-    forceSSL = true;
-    locations."/".proxyPass = "http://[::1]:${toString config.services.flaresolverr.port}";
   };
 }
