a94fee4134caa19185e06f6c1f9ad56996dfb6f7
ldesgoui <ldesgoui@gmail.com>
2025-01-17 15:13:19 +0100

    fix(knot): give knotc to install-knot-zones

--- computers/soldier/knot.nix
+++ computers/soldier/knot.nix
@@ -4,7 +4,7 @@ let
     name = "install-knot-zones";
     text = builtins.readFile ./install-knot-zones.sh;
 
-    runtimeInputs = [ pkgs.gawk ];
+    runtimeInputs = [ pkgs.gawk pkgs.knot-dns ];
     runtimeEnv = {
       KEEP_RECORDS_AWK = ./keep-records.awk;
       ZONES = inputs.self.packages.${pkgs.stdenv.hostPlatform.system}.dns-zones;
