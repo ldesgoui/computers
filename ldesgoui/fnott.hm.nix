2780702b60f5ef3932c8c3d0266601f40b8ba6f0
ldesgoui <ldesgoui@gmail.com>
2025-01-25 22:59:22 +0100

    Try to fix fnott settings

--- ldesgoui/fnott.hm.nix
+++ ldesgoui/fnott.hm.nix
@@ -7,9 +7,7 @@ _:
         title-font = "FiraMono Nerd Font:size=7";
         summary-font = "Work Sans:size=18:weight=Light";
         body-font = "PT Serif:size=9";
-      };
 
-      default = {
         default-timeout = 30;
         idle-timeout = 60;
       };
