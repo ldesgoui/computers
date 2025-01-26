8552ec1cf9aad98172680a353707b8830ce53fe7
ldesgoui <ldesgoui@gmail.com>
2025-01-26 19:06:42 +0100

    Fix vaultwarden SMTP settings

--- soldier/vaultwarden.os.nix
+++ soldier/vaultwarden.os.nix
@@ -63,7 +63,7 @@ in
       SSO_AUTHORITY = "https://auth.lde.sg/oauth2/openid/vaultwarden";
       SSO_CLIENT_ID = "vaultwarden";
 
-      SMTP_HOST = "localhost";
+      SMTP_HOST = "mx1.lde.sg";
       SMTP_SECURITY = "force_tls";
       SMTP_PORT = 465;
       SMTP_FROM = "vaultwarden@lde.sg";
