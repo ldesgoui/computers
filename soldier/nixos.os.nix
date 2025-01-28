a1be96d049e5746f6e3d1be64536dd6f5b8a801f
ldesgoui <ldesgoui@gmail.com>
2025-01-29 00:36:25 +0100

    Revert "force import zfs on soldier"
    This reverts commit f148005c189529bd1ab2b75cdbb2ac06afff8a28.

--- soldier/nixos.os.nix
+++ soldier/nixos.os.nix
@@ -4,7 +4,5 @@ _:
     hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8Kg9mUIe8h3RMjVJtXkTYK1cHxu5ZX8KHL/+EXlLhO";
   };
 
-  boot.zfs.forceImportRoot = true;
-
   users.users.ldesgoui.homeMode = "711"; # services need x
 }
