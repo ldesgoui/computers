f148005c189529bd1ab2b75cdbb2ac06afff8a28
ldesgoui <ldesgoui@gmail.com>
2025-01-29 00:00:09 +0100

    force import zfs on soldier

--- soldier/nixos.os.nix
+++ soldier/nixos.os.nix
@@ -4,5 +4,7 @@ _:
     hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH8Kg9mUIe8h3RMjVJtXkTYK1cHxu5ZX8KHL/+EXlLhO";
   };
 
+  boot.zfs.forceImportRoot = true;
+
   users.users.ldesgoui.homeMode = "711"; # services need x
 }
