4b87055bc4c279c6e04f6decfd4856a7b1136bbf
ldesgoui <ldesgoui@gmail.com>
2025-01-28 14:52:38 +0100

    Add authorizedKeys

--- ldesgoui/user.os.nix
+++ ldesgoui/user.os.nix
@@ -9,6 +9,15 @@ _:
     isNormalUser = true;
     hashedPasswordFile = config.age.secrets.ldesgoui-password.path;
     extraGroups = [ "wheel" ];
+
+    openssh = {
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK25ea20daUVvmTPmUL1nF/0DXEz/7tPBXOSerQNTf6+ ldesgoui@scout"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEKTIdCTmYMk+5MxwWLhH4YNDwY2zXIuEvnyzIrXikOe ldesgoui@soldier"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUtSdGOMvVecz0iJCx/IQxDxYEC6+TifOIF2wO+eRrf ldesgoui@sniper"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGuoIJZbzmxMq+noT7CGcQ8WxhtJN9dJeQ6HhTtPYsAy ldesgoui@recovery"
+      ];
+    };
   };
 
   zfs.datasets = {
