bffa7aac1883c373e2cbcdc15290731f23535b33
ldesgoui <ldesgoui@gmail.com>
2025-02-13 13:34:58 +0100

    remove block pool in sniper

--- sniper/storage.os.nix
+++ sniper/storage.os.nix
@@ -30,19 +30,4 @@ in
       compatibility = "openzfs-2.2-linux";
     };
   };
-
-  zfs.pools.block = {
-    vdevs = [ (bssd "6a149eb0-0a70-4c13-a6fa-584b6cf2a7b2") ];
-
-    properties = {
-      ashift = "12";
-      autoexpand = "on";
-      autotrim = "on";
-      compatibility = "openzfs-2.2-linux";
-    };
-  };
-
-  zfs.datasets.block = {
-    inherit (config.zfs.datasets.main) properties;
-  };
 }
