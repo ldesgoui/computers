a6cc6fb0737c0733af3a5363809c7c29a0a2fa27
ldesgoui <ldesgoui@gmail.com>
2025-07-11 20:49:22 +0200

    upgrade kanidm

--- soldier/kanidm.os.nix
+++ soldier/kanidm.os.nix
@@ -28,7 +28,7 @@ in
 
   services.kanidm = {
     enableServer = true;
-    package = pkgs.kanidmWithSecretProvisioning_1_5;
+    package = pkgs.kanidmWithSecretProvisioning_1_6;
 
     serverSettings = {
       inherit domain;
