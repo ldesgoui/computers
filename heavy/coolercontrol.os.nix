22fdeadedee5f6e1a913771ae18d26a7f98c6984
ldesgoui <ldesgoui@gmail.com>
2025-07-11 23:51:17 +0200

    don't use timer

--- heavy/coolercontrol.os.nix
+++ heavy/coolercontrol.os.nix
@@ -13,30 +13,17 @@ _:
 
   systemd.services."coolercontrol-lsi-temperature" = {
     script = ''
-      set -eux
-      ${pkgs.lsiutil}/bin/lsiutil -p1 -a 25,2,0,0 \
-        | ${lib.getExe pkgs.gawk} '/IOCTemperature: /{print strtonum($2)*1000}' \
-        > $RUNTIME_DIRECTORY/temp
+      set -eu
+      while sleep 1; do
+        ${pkgs.lsiutil}/bin/lsiutil -p1 -a 25,2,0,0 \
+          | ${lib.getExe pkgs.gawk} '/IOCTemperature: /{print strtonum($2)*1000}' \
+          > $RUNTIME_DIRECTORY/temp
+      done
     '';
 
     serviceConfig = {
       RuntimeDirectory = "coolercontrol-lsi-temperature";
-      Type = "oneshot";
-      User = "root";
-    };
-
-    unitConfig = {
-      StartLimitIntervalSec = 0;
-    };
-  };
-
-  systemd.timers."coolercontrol-lsi-temperature" = {
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      AccuracySec = "100ms";
-      OnBootSec = "1min";
-      OnUnitActiveSec = "1s";
-      Unit = "coolercontrol-lsi-temperature.service";
+      User = "root"; # TODO: don't use root
     };
   };
 }
