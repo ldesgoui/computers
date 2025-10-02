d7317b4c743a2c515d78679fd1f021f11dde66f2
ldesgoui <ldesgoui@gmail.com>
2025-10-02 15:47:44 +0200

    try unstable flaresolverr

--- soldier/media-server/flaresolverr.os.nix
+++ soldier/media-server/flaresolverr.os.nix
@@ -1,6 +1,11 @@
-_:
+{ inputs, ... }:
+{ pkgs, ... }:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { inherit (pkgs) config system; };
+in
 {
   services.flaresolverr = {
     enable = true;
+    package = pkgs-unstable.flaresolverr;
   };
 }
