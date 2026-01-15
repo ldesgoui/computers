8746608b4fd93a25255c872d9582b07da2c9de69
ldesgoui <ldesgoui@gmail.com>
2026-01-15 15:14:52 +0100

    install podman on scout

--- scout/nixos.os.nix
+++ scout/nixos.os.nix
@@ -5,6 +5,15 @@ _:
   };
 
   users.users.ldesgoui = {
-    extraGroups = [ "dialout" ];
+    extraGroups = [ "dialout" "podman" ];
+  };
+
+  virtualisation = {
+    containers.enable = true;
+    podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
+    };
   };
 }
