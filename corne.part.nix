6d4fbfb6632d7b25fafa71def04556a4b45f4c38
ldesgoui <ldesgoui@gmail.com>
2026-01-22 17:13:03 +0100

    corne with dongle

--- corne.part.nix
+++ corne.part.nix
@@ -10,9 +10,22 @@ _: {
         '';
 
         board = "nice_nano_v2";
-        shield = "splitkb_aurora_corne_%PART%";
+        shield = "corne_%PART%";
+        parts = [ "dongle" "p_left" "p_right" ];
 
-        enableZmkStudio = true;
+        zephyrDepsHash = "sha256-SHiCGErcstMH9EbvbQROXIhxFEbMf3AungYu5YvqMEg=";
+      };
+
+      reset-firmware = inputs'.zmk-nix.legacyPackages.buildKeyboard {
+        name = "reset-firmware";
+
+        src = pkgs.runCommand "corne-firmware-config" { } ''
+          mkdir $out
+          cp -r ${./corne} $out/config
+        '';
+
+        board = "nice_nano_v2";
+        shield = "settings_reset";
 
         zephyrDepsHash = "sha256-SHiCGErcstMH9EbvbQROXIhxFEbMf3AungYu5YvqMEg=";
       };
