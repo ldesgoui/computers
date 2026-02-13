935e5b2a117f078e887fcf6ef816c00695f21856
ldesgoui <ldesgoui@gmail.com>
2026-02-13 02:37:21 +0100

    install greetd

---
+++ ldesgoui/greetd.os.nix
@@ -0,0 +1,11 @@
+_:
+{ pkgs, ... }: {
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.greetd}/bin/agreety --cmd niri-session";
+      };
+    };
+  };
+}
