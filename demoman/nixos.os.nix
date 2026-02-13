95ad9b7f2772e39ef1285e0e5ba7f8aaf9d8c83f
ldesgoui <ldesgoui@gmail.com>
2026-02-13 20:51:16 +0100

    fix

--- demoman/nixos.os.nix
+++ demoman/nixos.os.nix
@@ -1,6 +1,6 @@
 _:
 { lib, pkgs, ... }: {
   age.rekey = {
-    # hostPubkey = "ssh-ed25519 ";
+    hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICXuFP+SkQRUiLBnvY02eKLoRN8SfjyZAmXKm/mm/Rhr";
   };
 }
