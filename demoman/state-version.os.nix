00d8e03c196fb79f327d5851d22aa343376cfc44
ldesgoui <ldesgoui@gmail.com>
2026-02-13 19:21:47 +0100

    fix

--- demoman/state-version.os.nix
+++ demoman/state-version.os.nix
@@ -1,7 +1,7 @@
 _:
 {
-  system.stateVersion = "25.11";
+  system.stateVersion = "25.05";
   home-manager.users.ldesgoui = {
-    home.stateVersion = "25.11";
+    home.stateVersion = "25.05";
   };
 }
