a8947fe5cdfef47b3fe3c0d4cbb9831a297c8012
ldesgoui <ldesgoui@gmail.com>
2026-02-14 01:04:37 +0100

    update

--- demoman/slop.os.nix
+++ demoman/slop.os.nix
@@ -1,12 +1,16 @@
-_:
-{ pkgs, ... }: {
+{ inputs, ... }:
+{ pkgs, ... }:
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { inherit (pkgs) config system; };
+in
+{
   environment.systemPackages = [
     pkgs.claude-code
   ];
 
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-rocm;
+    package = pkgs-unstable.ollama-rocm;
     rocmOverrideGfx = "10.3.0";
   };
 }
