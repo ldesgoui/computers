507ea3bf516a44ded142783d226b8591ca75b42d
ldesgoui <ldesgoui@gmail.com>
2026-06-06 14:13:01 +0200

    remove heavy as it's not deployed

--- heavy/nixos.os.nix
+++
@@ -1,6 +0,0 @@
-_:
-{ lib, ... }: {
-  age.rekey = {
-    hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOl9I0HzHom5YxcealhIHeX4M9tWQeLn53SFBDywNd/f";
-  };
-}
--- heavy/tailscale.os.nix
+++
@@ -1,6 +0,0 @@
-_:
-{
-  services.tailscale = {
-    enable = true;
-  };
-}
