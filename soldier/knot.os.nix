5fd6337dc307604cb8dad8d44c80f8ac76391b6e
ldesgoui <ldesgoui@gmail.com>
2026-06-06 15:49:15 +0200

    deploy zones differently

--- soldier/knot.os.nix
+++ soldier/knot.os.nix
@@ -1,16 +1,7 @@
 { self, ... }:
 { lib, pkgs, ... }:
 let
-  install-knot-zones = pkgs.writeShellApplication {
-    name = "install-knot-zones";
-    text = builtins.readFile ./install-knot-zones.sh;
-
-    runtimeInputs = [ pkgs.gawk pkgs.knot-dns ];
-    runtimeEnv = {
-      KEEP_RECORDS_AWK = ./keep-records.awk;
-      ZONES = self.packages.${pkgs.stdenv.hostPlatform.system}.dns-zones;
-    };
-  };
+  zones = self.packages.${pkgs.stdenv.hostPlatform.system}.dns-zones;
 in
 {
   networking.firewall = {
@@ -35,17 +26,29 @@ in
         ];
       };
 
-      acl = [{
-        id = "update-txt-only";
-        address = [
-          "127.0.0.1"
-          "::1"
-          "100.101.0.0/24"
-          "fd7a:115c:a1e0::/112"
-        ];
-        action = "update";
-        update-type = [ "TXT" ];
-      }];
+      acl = [
+        {
+          id = "axfr-local";
+          address = [
+            "127.0.0.1"
+            "::1"
+            "100.101.0.0/24"
+            "fd7a:115c:a1e0::/112"
+          ];
+          action = "transfer";
+        }
+        {
+          id = "update-txt-only";
+          address = [
+            "127.0.0.1"
+            "::1"
+            "100.101.0.0/24"
+            "fd7a:115c:a1e0::/112"
+          ];
+          action = "update";
+          update-type = [ "TXT" ];
+        }
+      ];
 
       policy = [{
         id = "sign-ed25519";
@@ -55,7 +58,8 @@ in
 
       template = [{
         id = "default";
-        acl = "update-txt-only";
+        file = "${zones}/%s.zone";
+        acl = [ "axfr-local" "update-txt-only" ];
         dnssec-signing = "on";
         dnssec-policy = "sign-ed25519";
         semantic-checks = "on";
@@ -82,10 +86,6 @@ in
       ];
     };
   };
-
-  system.activationScripts."install-knot-zones".text = ''
-    ${lib.getExe install-knot-zones}
-  '';
 
   zfs.datasets.main = {
     enc.services.knot = {
