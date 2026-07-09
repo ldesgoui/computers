54640b633a6692837f05f5098cbaca3c104d545b
ldesgoui <ldesgoui@gmail.com>
2026-07-09 02:14:44 +0200

    acme-tsig: fix ns, move out of secret

--- nixos/acme-tsig.nix
+++ nixos/acme-tsig.nix
@@ -3,7 +3,7 @@
     let
       inherit (config.networking) hostName;
 
-      nameserver = "2001:41d0:fc14:cafe::ff:fe00:53";
+      nameserver = "[2001:41d0:fc14:cafe::ff:fe00:53]";
       physNames = [ "scout" "soldier" "pyro" "demoman" "heavy" "engineer" "medic" "sniper" "spy" ];
       dir = if builtins.elem hostName physNames then "phys" else "virt";
     in
@@ -22,7 +22,6 @@
             ${decrypt} ${lib.escapeShellArg deps.acme-tsig.file} \
             | ${pkgs.yq-go}/bin/yq eval -o=shell '
               .key[0] | {
-                "RFC2136_NAMESERVER": "${nameserver /* would this break on knot-primary? */}",
                 "RFC2136_TSIG_KEY": .id,
                 "RFC2136_TSIG_ALGORITHM": .algorithm,
                 "RFC2136_TSIG_SECRET": .secret
@@ -39,6 +38,9 @@
 
           dnsProvider = "rfc2136";
           environmentFile = config.age.secrets.acme-tsig-env.path;
+          credentialFiles = {
+            RFC2136_NAMESERVER_FILE = builtins.toFile "nameserver.txt" nameserver;
+          };
 
           dnsResolver = "1.1.1.1:53";
         };
