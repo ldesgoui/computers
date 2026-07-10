473cffa06a453709a172c1ae8194b63bdeef2fb4
ldesgoui <ldesgoui@gmail.com>
2026-07-10 04:37:42 +0200

    http-proxy: fix haproxy config

--- virt/http-proxy/default.nix
+++ virt/http-proxy/default.nix
@@ -53,6 +53,12 @@
               daemon
               maxconn 2048
 
+            defaults
+                timeout connect 5s
+                timeout client 30s
+                timeout server 30s
+                timeout tunnel 4h
+
             resolvers dns
                 parse-resolv-conf
                 resolve_retries 3
@@ -65,11 +71,12 @@
                 mode http
 
                 acl allowed_host hdr(host),field(1,:) -i -m dom -f /etc/haproxy/allowed-domains
-                http-request deny deny_status 400 if !allowed_host
+                http-request deny deny_status 400 unless allowed_host
 
                 http-request do-resolve(txn.dst,dns,ipv6) hdr(host),field(1,:)
-                use_backend be_http if { var(txn.dst) -m found }
-                http-request deny deny_status 400
+                http-request deny deny_status 400 unless { var(txn.dst) -m found }
+
+                use_backend be_http
 
             backend be_http
                 mode http
@@ -79,15 +86,17 @@
             frontend https
                 bind *:443
                 mode tcp
+
                 tcp-request inspect-delay 5s
                 tcp-request content accept if { req.ssl_hello_type 1 }
 
                 acl allowed_sni req.ssl_sni -i -m dom -f /etc/haproxy/allowed-domains
-                tcp-request content reject if !allowed_sni
+                tcp-request content reject unless allowed_sni
 
                 tcp-request content do-resolve(txn.dst,dns,ipv6) req.ssl_sni
-                use_backend be_https if { var(txn.dst) -m found }
-                tcp-request content reject
+                tcp-request content reject unless { var(txn.dst) -m found }
+
+                use_backend be_https
 
             backend be_https
                 mode tcp
