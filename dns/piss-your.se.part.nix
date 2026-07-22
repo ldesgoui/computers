431bcbd3c6bc1a96031d34554a7d8cae4b438552
ldesgoui <ldesgoui@gmail.com>
2026-07-22 23:25:24 +0200

    Revert "piss-your.se: remove ipv6 from glue for now"
    This reverts commit 9eb3b1b65985cddb4aa62e191803152f108d97d7.

--- dns/piss-your.se.part.nix
+++ dns/piss-your.se.part.nix
@@ -47,9 +47,7 @@ in
     }];
 
     subdomains = {
-      ns1 = {
-        inherit (wi.soldier) A; # Must be glue
-      };
+      ns1 = wi.soldier; # Must be glue
       ns2 = wi.sniper; # Must be glue
 
       autoconfig.CNAME = [ "mx1.lde.sg." ];
