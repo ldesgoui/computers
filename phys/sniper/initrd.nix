614b21be78fd86b071036bd31394a41d37ebcbf0
ldesgoui <ldesgoui@gmail.com>
2026-08-05 03:46:36 +0200

    sniper: enable initrd ssh

--- phys/sniper/initrd.nix
+++ phys/sniper/initrd.nix
@@ -1,30 +1,41 @@
-{
+{ config, ... }: {
   boot.initrd = {
-    # network = {
-    #   enable = true;
+    network = {
+      enable = true;
 
-    #   ssh = {
-    #     enable = true;
-    #     port = 2222;
-    #     hostKeys = [ "/etc/ssh/host-keys/initrd_ed25519" ];
-    #     authorizedKeys = config.users.users.root.openssh.authorizedKeys.keys;
-    #   };
-    # };
+      ssh = {
+        enable = true;
+        port = 2222;
+        hostKeys = [ "/etc/ssh/host-keys/initrd_ed25519" ];
+        authorizedKeys = config.users.users.root.openssh.authorizedKeys.keys;
+      };
+    };
 
-    systemd.enable = true;
+    systemd = {
+      enable = true;
+      emergencyAccess = true; # TODO: password
 
-    systemd.emergencyAccess = true;
+      network.networks."10-ens2" = {
+        matchConfig.Name = "ens2";
+        networkConfig = {
+          DHCP = "ipv4";
+        };
+        ipv6AcceptRAConfig = {
+          Token = "::1:1";
+        };
+      };
 
-    systemd.services.symlink-machine-id = {
-      after = [ "var-lib-systemd.mount" ];
-      before = [ "initrd-switch-root.target" ];
+      services.symlink-machine-id = {
+        after = [ "var-lib-systemd.mount" ];
+        before = [ "initrd-switch-root.target" ];
 
-      serviceConfig.Type = "oneshot";
+        serviceConfig.Type = "oneshot";
 
-      script = ''
-        mkdir -p /sysroot/etc
-        ln -sf /var/lib/systemd/machine-id /sysroot/etc/machine-id
-      '';
+        script = ''
+          mkdir -p /sysroot/etc
+          ln -sf /var/lib/systemd/machine-id /sysroot/etc/machine-id
+        '';
+      };
     };
   };
 }
