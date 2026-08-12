9d8036d9af64266f4a93381d62855d7adceb2fe8
ldesgoui <ldesgoui@gmail.com>
2026-08-13 00:27:02 +0200

    dns: take out old email records

--- dns/piss-your.se.part.nix
+++ dns/piss-your.se.part.nix
@@ -18,7 +18,6 @@ in
 
     CAA = dns.lib.letsEncrypt "ldesgoui@gmail.com";
 
-
     A = [
       "212.47.233.201"
       # "109.190.105.250"
@@ -27,31 +26,6 @@ in
       "2001:bc8:710:7dfc::8"
       # "2001:41d0:fc14:cafe::ff:fe07:ebe9"
     ];
-
-    MX = [{ exchange = "mx1.lde.sg."; preference = 10; }];
-
-    TXT = [ (dns.lib.spf.strict [ "mx ra=postmaster" ]) ];
-
-    DKIM = [
-      {
-        selector = "202501e";
-        k = "ed25519";
-        h = [ "sha256" ];
-        p = "mAujEmbxvPSofnocQMfcjz10kIHyRFB3nOde8B0SePY=";
-      }
-      {
-        selector = "202501r";
-        k = "rsa";
-        h = [ "sha256" ];
-        p = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwoBnSBD3TrRzddkxPyTXEEGsi3X4LdBJSbpJ7kfmiaDQNiet7p9d6vmaZYasRXZcT/wGLqld7+pIaztXMM8Sn/UHuhN+TDxYAax71eAmqIXj+HGqsx87RM1NUVSNsABQI/xFD3aqwe/Q69XCGnGfW7/fKaqwXTRU0qAoDi2/AxRWDs/eFfDfj/Mw+c7De+ojhMuX5rc8IQCU9J/GF2/zCKEbfPWLUT7NYnfH2vZ/Prisp1VcS61GCq6ygveP4SKEKIv6Kn1v8NV1VO0vBqBQLbgNiO/eE+H52xVnyc5/14S0lnZY2POlcnFqVFg6HIaCjoU2c+JeBO3p3b/vzyRUSQIDAQAB";
-      }
-    ];
-
-    DMARC = [{
-      p = "reject";
-      rua = "mailto:postmaster@piss-your.se";
-      ruf = [ "mailto:postmaster@piss-your.se" ];
-    }];
 
     subdomains = {
       ns1 = {
@@ -64,10 +38,6 @@ in
       hosts.subdomains = {
         sniper = wi.sniper;
       };
-
-      autoconfig.CNAME = [ "mx1.lde.sg." ];
-
-      _tls.subdomains._smpt.TXT = [{ data = "v=TLSRPTv1; rua=mailto:postmaster@lde.sg"; }];
     };
   };
 }
--- dns/tf2.spot.part.nix
+++ dns/tf2.spot.part.nix
@@ -25,37 +25,7 @@ in
     CAA = dns.lib.letsEncrypt "ldesgoui@gmail.com";
 
     inherit (to-vm) A AAAA;
-
-    MX = [{ exchange = "mx1.lde.sg."; preference = 10; }];
-
-    TXT = [ (dns.lib.spf.strict [ "mx ra=postmaster" ]) ];
-
-    DKIM = [
-      {
-        selector = "202509e";
-        k = "ed25519";
-        h = [ "sha256" ];
-        p = "ooPxqxxfCvM+oCQF/o+OGlJYOY6Ui2tnuP2Bwb2ud/I=";
-      }
-      {
-        selector = "202509r";
-        k = "rsa";
-        h = [ "sha256" ];
-        p = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwklM5belYHXH1m5WGhdoCbl+NGWBfS9Vi4ihh2xcxC86x+znmGwfSZYF3eFDyMqfo4tSI8SBgD6RMtRkGqM9zLtszDOMhnQzk12wpR6Vi/2LcqnN0TX0K/WYV03Wnb+SYZL6xPKgzIOZgqO3sdacEx6Uo6C5n0yKng2u3QUhZV5Ot2DE2G1knV9uF7jFqfJn3YaV6jY2zN9wWk4l+liWN4B50pp9KEOgzhwIUJIr42G5rxi/0B2Lj+TjhxII0tugUf1goyy4hv6O3eRw+VqJTS36GXgRuxkXM5dOMmg5cm9SELN1vctRpzvBJ9Vo/KajtM8TIvzmGBKQkHddEIkU2QIDAQAB";
-      }
-    ];
-
-    DMARC = [{
-      p = "reject";
-      rua = "mailto:postmaster@tf2.spot";
-      ruf = [ "mailto:postmaster@tf2.spot" ];
-    }];
-
     subdomains = {
-      autoconfig.CNAME = [ "mx1.lde.sg." ];
-
-      _tls.subdomains._smpt.TXT = [{ data = "v=TLSRPTv1; rua=mailto:postmaster@tf2.spot"; }];
-
       fantasy = to-vm;
       # postgrest = to-vm; # TODO: make postgrest public
       mathesar = to-vm;
